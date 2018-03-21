1b462e79af43dcca5e9b8acf3e77246bd4102d6a
Your Name <you@example.com>
2018-03-21 16:44:17 +0200

    commit

---
+++ kodikas.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+public class Player
+{
+	string name;
+	int age;
+	string sex;
+	string job;
+	float texnikiKatartisi;
+	float kourasi;
+	float popularity;
+	
+}
+
+public class Team
+{
+	string name;
+	int numberOfPlayers;
+	Player paiktes[];
+		
+}
+
+
+public class Diagonismos
+{
+	string type;
+	string kodikos;
+	
+}
+
+
